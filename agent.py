7d687e06ac3ce7f3a6590da8ac4e6d9fbdcc675f
Christopher Torrington <ctorrington1@gmail.com>
2023-09-25 18:34:59 +0100

    state value function. NO DELTA

--- agent.py
+++ agent.py
@@ -10,11 +10,10 @@ class Agent:
 
     def __init__(self) -> None:
         self.grid_world = GridWorld()
-
         self.policy = "random"
+        self.gamma = 1
 
-    def _get_action_probability_distribution(self, state: tuple[int, int]) -> dict[ACTIONS,
-                                                                      int]:
+    def _get_action_probability_distribution(self, state: tuple[int, int]) -> dict[ACTIONS, float | int]:
         """
         Return the probability distribution of taken each action
         available to the given state.
@@ -36,3 +35,33 @@ class Agent:
             pass
 
         return action_probability_distribution
+    
+    def _determine_state_value(self, state: tuple[int, int]) -> float | int:
+        """Return the estimated state value for the given state."""
+
+        state_value = self.grid_world._get_state_return(state)
+
+        action_probability_distribution = self._get_action_probability_distribution(state)
+
+        for action in action_probability_distribution:
+            action_probability = action_probability_distribution[action]
+            next_state = self.grid_world._get_next_state(state, action)
+            reward = self.grid_world._get_state_reward(next_state)
+
+            state_value += (action_probability * self.grid_world._get_state_transition_probability(state, action) * (reward + (self.gamma * self.grid_world._get_state_return(next_state))))
+
+        return state_value
+
+    def determine_state_value_over_state_space(self) -> None:
+        """
+        Determine the state values of all states within the given state space.
+        """
+
+        for state in self.grid_world.state_space:
+            self.grid_world._set_state_return(state,
+                                              self._determine_state_value(state))
+            
+        # Reset the return for the terminal states - should should not have any.
+        # Doing it like this, rather than with an if statement, for performance.
+        for state in self.grid_world._get_terminal_states():
+            self.grid_world._set_state_return(state, 0)
