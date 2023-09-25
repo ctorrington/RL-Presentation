8d41001d53304df83ec2445bb55f0acc2fe5a80d
Christopher Torrington <ctorrington1@gmail.com>
2023-09-25 12:40:40 +0100

    get action probability distribution for agent class

--- agent.py
+++ agent.py
@@ -1,6 +1,9 @@
 """RL Agent to interact with a given environment."""
 
 from grid_world import GridWorld
+from constants import Constants
+
+ACTIONS = Constants.ACTIONS
 
 class Agent:
     """RL Agent."""
@@ -9,4 +12,25 @@ class Agent:
         self.grid_world = GridWorld()
 
         self.policy = "random"
-        print(self.grid_world._get_available_actions((0, 0)))
+
+    def _get_action_probability_distribution(self, state: tuple[int, int]) -> dict[ACTIONS,
+                                                                      int]:
+        """Return the probability distribution of taken each action
+        available to the given state."""
+
+        action_probability_distribution = {}
+
+        # Random policy.
+        if self.policy == "random":
+            # The agent follows a random policy.
+            # All actions are chosen equally randomly.
+            available_actions = self.grid_world._get_available_actions(state)
+            action_probability = 1 / len(available_actions)
+            for action in available_actions:
+                action_probability_distribution[action] = 1 / len(available_actions)
+
+        # Other policies. (future work)
+        else:
+            pass
+
+        return action_probability_distribution
