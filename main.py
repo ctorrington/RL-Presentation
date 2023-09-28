179acf6f82e94e4defec2cb50ae305ac700db019
ctorrington1 <ctorrington1@gmail.com>
2023-09-28 17:05:07 +0100

    action arrow plotting

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     agent.determine_state_value_over_state_space()
     agent.grid_world.print_state_space()
-    agent.get_environment()
+    # agent.get_environment()
 
     plotter = Plotter()
     plotter.plot_animation(agent)
--- plotter.py
+++ plotter.py
@@ -1,6 +1,7 @@
 """Plot data."""
 
 from agent import Agent
+from grid_world import GridWorld
 from constants import Constants
 
 import matplotlib.pyplot as plt
@@ -8,12 +9,13 @@ import matplotlib.animation as mpla
 import numpy as np
 
 ACTIONS = Constants.ACTIONS
+State = Constants.State
 
 class Plotter:
     def __init__(self) -> None:
         pass
 
-    def create_z_axis_data(self, data) -> list[list[int]]:
+    def create_z_axis_data(self, data: GridWorld, ax) -> list[list[int]]:
         """Return the data for the imshow function in the correct format."""
 
         # print(data.print_state_space())
@@ -31,60 +33,77 @@ class Plotter:
                 else:
                     # Otherwise append state return value.
                     row.append(data.state_space[state]["return"])
+                    
+                # Add the annotations for action directions.
+                self.annotate(state, data, ax)
+                
             data_matrix.append(row)
 
         return data_matrix
     
+    def annotate(self, state: State,
+                 environment: GridWorld,
+                 ax) -> None:
+        """Add annotations to the plot."""
+        
+        arrow_start_position = (state[0], state[1])
+        arrow_directions = list[ACTIONS]
+        
+        if len(environment._get_available_actions(state)) > 0:
+            print(state)
+            print(environment._get_available_actions(state))
+            arrow_directions = environment._get_available_actions(state)
+        else:
+            # No available actions for the state.
+            return
+        
+        for direction in arrow_directions:
+            match direction:
+                case ACTIONS.UP:
+                    xytext = (state[0], state[1] + 1)
+
+                case ACTIONS.DOWN:
+                    xytext = (state[0], state[1] - 1)
+
+                case ACTIONS.LEFT:
+                    xytext = (state[0] - 1, state[1])
+
+                case ACTIONS.RIGHT:
+                    xytext = (state[0] + 1, state[1])                
+
+                case _:
+                    print("I should I written a proper error message.")
+                    return
+                
+        ax.annotate("",
+                    xy = arrow_start_position,
+                    xytext = xytext,
+                    arrowprops = dict(arrowstyle = "<-",
+                                    color = "orange"))
+        
+    
     def plot_animation(self, agent: Agent) -> None:
 
-        data = agent.history
+        history: list[GridWorld] = agent.history
 
         fig, ax = plt.subplots()
-        plot_data = ax.imshow(self.create_z_axis_data(data[0]))
-
-        def annotate(i):
-            # For every state in the environment.
-            for state in data[i].state_space:
-                arrow_direction = (state[0], state[1])
-                # Catch list index out of range error for no avialable actions.
-                # Check that the list is not empty.
-                if len(data[i]._get_available_actions(state)) > 0:
-                    action_direction = data[i]._get_available_actions(state)[0]
-                else:
-                    # If the list is empty, don't draw an arrow.
-                    continue
-
-                match action_direction:
-                    case ACTIONS.UP:
-                        xytext = (state[0] - 1, state[1])
-
-                    case ACTIONS.DOWN:
-                        xytext = (state[0] + 1, state[1])
-
-                    case ACTIONS.LEFT:
-                        xytext = (state[0], state[1] - 1)
-
-                    case ACTIONS.RIGHT:
-                        xytext = (state[0], state[1] + 1)                
-
-                    case _:
-                        xytext = (state[0]. state[1])
-
-                ax.annotate("",
-                            xy = arrow_direction,
-                            xytext = xytext,
-                            arrowprops = dict(arrowstyle = "->",
-                                            color = "black"))
+        ax.set_xlim(0, agent.grid_world.get_number_of_columns())
+        ax.set_ylim(0, agent.grid_world.get_number_of_rows())
+        
+        plot_data = ax.imshow(self.create_z_axis_data(history[0], ax))
 
         def animate(i):
-            plot_data.set_data(self.create_z_axis_data(data[i]))
+            # Plot the state space.
+            plot_data.set_data(self.create_z_axis_data(history[i], ax))
+            
+            # Annotate the arrows to show direction for each state.
             # annotate(i)
 
         anim = mpla.FuncAnimation(
             plt.gcf(),
             animate,
-            frames = len(data),
-            interval = 5,
+            frames = len(history),
+            interval = 100,
             repeat = True
         )
         plt.show()
